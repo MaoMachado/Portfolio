e25beb86bed8b6958dba1f2667c754dd26946c9d
MaoMachado <114309283+MaoMachado@users.noreply.github.com>
2026-05-19 11:52:49 -0500

    feat: Implementar el diseño principal con componentes de encabezado y pie de página reutilizables.

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white text-dark dark:bg-gray-900 dark:text-white transition-colors duration-300">
+      <div className="flex items-center justify-center gap-10 p-6 lg:px-8">
+        <p className="text-sm/6 font-semibold">2026 © Mao.Dev</p>
+        <a href="#">
+          <p className="text-sm/6 font-semibold">machado-mario@outlook.com</p>
+        </a>
+      </div>
+    </footer>
+  );
+}
