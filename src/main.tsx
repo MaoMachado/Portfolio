e46dc25642ea9c8d157e125f20022bb6d0c35e62
MaoMachado <114309283+MaoMachado@users.noreply.github.com>
2026-05-20 18:21:44 -0500

    feat: actualizar react-router-dom a la versión 7.15.1 y agregar importación del componente Projects en las rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import { RouterProvider } from "react-router-dom";
+
 import { router } from "./pages/routes.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
+
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
